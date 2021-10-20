a11c90ea1600aceab118e2bb6e52cc179caacbaf
RamitaShrestha <92393788+RamitaShrestha@users.noreply.github.com>
2021-10-20 13:23:40 -0400

    Update exercise6.py
    I have added doc strings.

--- exercise6.py
+++ exercise6.py
@@ -1,5 +1,19 @@
+"""
+Driver: Nikole Grandez 
+Navigator: Ramita Shrestha
+Assignment: Exercise 6
+Date:10_20_21
+Challenges Encountered: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+"""
+This function will great
+"""
 def helloWorld():
     print("hello world")
 
+"""
+This function will say bye
+"""
 def goodbyeWorld():
     print("Goodbye")
